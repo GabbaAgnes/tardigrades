73242be50d9c94ea39504feaf8dfa21684f2aca7
Agnes <gabbaagnes@gmail.com>
2018-11-15 11:00:06 -0800

    pineapplebreath

--- api.js
+++ api.js
@@ -1,11 +1,21 @@
-console.log("hello")
-let searchterm= "tardigrades"
-fetch(`http://api.giphy.com/v1/gifs/search?q=${searchterm}&api_key=IFi4RzT7NxvWjFC845z185zLEwjfObX5&limit=5`)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-    $(".gif").html(`<iframe width="480" height="300" src="${myJson.data[0].embed_url}"></iframe>`)
-  });
+
+function getPics(v){
+  fetch(`https://api.unsplash.com/search/photos?query=${v}&client_id=26466f28b131dd4faf1de40f6fdad48db8620e6172ff77f8c0b459e6ea421d2f`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        $(".gif").html('')
+        for (let i=0; i<5; i++) {
+          $(".gif").append(`<img src= ${myJson.results[i].urls.regular}/>`)
+        }
+       
+    });
+}
+       
+   
   
+  $('select').on('change', function() {
+    getPics(this.value);
+  });
